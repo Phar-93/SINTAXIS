18b1652d22c59f13c7d3e299d4e7a5b4dc26b10e
Ricardo <Phar-aza@hotmail.com>
2019-10-14 17:23:15 +0200

    primer commit

---
+++ js/prmerscript.js
@@ -0,0 +1,7 @@
+//alert("Hola mundo");
+confirm("Hola mundo");
+prompt("Cual es tu nombre?");
+document.write(" Hola me llamo Ricardo ");
+console.log("Esto es la pagina web de ricardo");
+console.warn("Esto es una advertencia");
+console.error("Error, por payaso");
