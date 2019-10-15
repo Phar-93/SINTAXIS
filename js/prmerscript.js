5e47a0001840bddf903c78045e6ab8f4b0e021c3
Ricardo <Phar-aza@hotmail.com>
2019-10-15 18:50:05 +0200

    commit2

--- js/prmerscript.js
+++ js/prmerscript.js
@@ -4,4 +4,4 @@ prompt("Cual es tu nombre?");
 document.write(" Hola me llamo Ricardo ");
 console.log("Esto es la pagina web de ricardo");
 console.warn("Esto es una advertencia");
-console.error("Error, por payaso");
+console.error("Error");
