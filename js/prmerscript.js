1517c7f0b654024ff1b31849732069e219f60c1d
Ricardo <Phar-aza@hotmail.com>
2019-10-16 20:58:02 +0200

    EJERCICIO 5.1.

--- js/prmerscript.js
+++ js/prmerscript.js
@@ -6,3 +6,4 @@ document.write(" Hola Mundo! ");
 console.log("Esto es la pagina web de ricardo");
 console.warn("Esto es una advertencia");
 console.error("Error");
+document.getElementById("celda1").innerHTML="Estados Unidos";
