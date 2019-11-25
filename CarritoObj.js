3a4d151682cd6c4f58cd1432d03f8e3574eab723
unknown <phar-aza@hotmail.com>
2019-11-25 09:31:35 +0100

    Modificaciones

--- CarritoObj.js
+++ CarritoObj.js
@@ -1,24 +1,25 @@
 
 
+var A = Array();
 
-
-function Carrito(uni, pre, cant){
-    this.uni=uni;
-   this.pre=pre;
-   this.cant=cant;
+function Carrito(uni, pre){
+    this.uni=uni || 1;
+   this.pre=pre || 50;
   };
 
-//   Carrito.prototype.toString = function precioToString() {
-//     var precio= new Carrito(pre, uni, cant);
-//     var retorno = this.uni+ this.pre + this.cant;
-//     return retorno;
-//   }
-  
 function Mostrar(){
+
 var pre = document.getElementById('pre').value;
 var uni = document.getElementById("uni").value;
 var cant = uni*pre;
     var precio= new Carrito(pre, uni, cant);
-     document.write(precio.pre," precio ",precio.uni," cantidad " ,precio.cant, " total \€ ");
+    A.push(uni);
+    document.getElementById("resul").innerHTML="";
+    A.forEach(function (elemento, indice, array) {
+        document.getElementById("resul").innerHTML+="El numero de unidades es: "+ A[indice]+"<br>";
+    });
+  
+
+
 }
   
